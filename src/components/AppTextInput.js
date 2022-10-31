f912bea8f1146b87365499a155d2b170556c83bd
Saar Berenstein <saarbm25@gmail.com>
2022-10-31 16:47:50 +0200

    added secure text feature to AppTextInput

--- src/components/AppTextInput.js
+++ src/components/AppTextInput.js
@@ -1,17 +1,23 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { colors } from '../global/styles'
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function AppTextInput({children, placeholder, icon, secure}) {
+export default function AppTextInput({ children, placeholder, icon, secure }) {
+    const [visable, useVisable] = useState(secure);
+    const changeVisibility = () => useVisable(!visable);
   return (
       <View style={styles.container}>
           {icon && <MaterialCommunityIcons name={icon} size={22} color={colors.grey3} style={styles.icon}></MaterialCommunityIcons>}
           <TextInput
               style={styles.inputText}
               placeholder={placeholder}
-          >{children}</TextInput>
-          {secure && <MaterialCommunityIcons name={'eye'} size={22} color={colors.grey3} style={styles.secure}></MaterialCommunityIcons>}
+              secureTextEntry={visable}
+          >{visable}</TextInput>
+          <TouchableOpacity
+              onPress={changeVisibility}>
+              {secure && <MaterialCommunityIcons name={'eye'} size={22} color={colors.grey3} style={styles.secure}></MaterialCommunityIcons>}  
+          </TouchableOpacity>
     </View>
   )
 }
