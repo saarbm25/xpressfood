ad7adbdca881840667424bb5ae3879ec58167c74
Saar Berenstein <saarbm25@gmail.com>
2022-10-31 14:56:06 +0200

    first video

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Header from './src/components/Header';
+import React from 'react';
+import { colors } from './src/global/styles';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor={colors.statusBar}></StatusBar>
+      <Header title='MY ACCOUNT'></Header>
     </View>
   );
 }
@@ -13,8 +18,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimentions, TouchableHighlight, TouchableOpacity } from 'react-native'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { colors, parameters } from '../global/styles' 
+
+function Header({title}) {
+    return (
+            <TouchableOpacity style={styles.header}>
+                <MaterialCommunityIcons name='arrow-left' size={28} color={colors.grey5}></MaterialCommunityIcons>
+                <View>
+                    <Text style={styles.headerText}>
+                        {title}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        backgroundColor: colors.button,
+        height: parameters.headerHeight,
+        alignItems: 'center',
+        paddingLeft: 10,
+    },
+    headerText: {
+        color: colors.grey5,
+        fontWeight: 'bold',
+        fontSize: 22,
+        marginLeft: 30,
+    }
+})
+
+export default Header;
